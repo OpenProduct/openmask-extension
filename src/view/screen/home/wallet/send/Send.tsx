06b8842c3046400b04d2ca590296118fad924234
Nikita Kuznetsov <nkuz915@gmail.com>
2022-08-27 00:45:55 +0200

    Edit send state button

--- src/view/screen/home/wallet/send/Send.tsx
+++ src/view/screen/home/wallet/send/Send.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useMemo } from "react";
+import React, { FC, useCallback, useMemo } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 import {
@@ -7,14 +7,49 @@ import {
   ButtonNegative,
   ButtonPositive,
   Center,
+  Container,
   Gap,
   H1,
   H3,
   Input,
   Text,
 } from "../../../../components/Components";
+import { BackIcon } from "../../../../components/Icons";
 import { LoadingLogo } from "../../../../components/Logo";
 import { AppRoute } from "../../../../routes";
+
+const Block = styled(Container)`
+  width: 100%;
+  padding-bottom: 0;
+`;
+
+const Button = styled.div`
+  cursor: pointer;
+`;
+
+const toState = (searchParams: URLSearchParams): State => {
+  return {
+    address: searchParams.get("address") ?? "",
+    amount: searchParams.get("amount") ?? "",
+    max: searchParams.get("max") ?? "",
+    comment: searchParams.get("comment") ?? "",
+  };
+};
+
+const EditButton = React.memo(() => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const state = toState(searchParams);
+  const onEdit = () => {
+    setSearchParams({ ...state });
+  };
+  return (
+    <Block>
+      <Button onClick={onEdit}>
+        <BackIcon /> Edit
+      </Button>
+    </Block>
+  );
+});
 
 const CancelButton: FC<{ disabled?: boolean }> = ({ disabled }) => {
   const navigate = useNavigate();
@@ -24,10 +59,6 @@ const CancelButton: FC<{ disabled?: boolean }> = ({ disabled }) => {
     </ButtonNegative>
   );
 };
-
-const Block = styled.div`
-  padding-bottom: ${(props) => props.theme.padding};
-`;
 
 const Label = styled.div`
   margin: ${(props) => props.theme.padding} 0 5px;
@@ -49,11 +80,6 @@ const MaxButton = styled.button`
   outline: 0;
 `;
 
-enum SendRoutes {
-  index = "/",
-  confirm = "/confirm",
-}
-
 interface Props {
   price?: number;
   balance?: string;
@@ -72,6 +98,7 @@ interface InputProps {
   onChange: (field: Partial<State>) => void;
   onSend: () => void;
 }
+
 const InputView: FC<InputProps> = ({ state, balance, onChange, onSend }) => {
   return (
     <Body>
@@ -121,22 +148,27 @@ interface ConfirmProps {
 
 const ConfirmView: FC<ConfirmProps> = ({ state, onConfirm }) => {
   return (
-    <Body>
-      <H1>Send TON</H1>
-      <Text>Send {state.amount} TON to</Text>
-      <Text>
-        <Address>{state.address}</Address>
-      </Text>
-      {state.comment && <Text>Comment: "{state.comment}"</Text>}
+    <>
+      <EditButton />
+      <Body>
+        <H3>Send TON</H3>
+        <Text>
+          Send <b>{state.amount}</b> TON to
+        </Text>
+        <Text>
+          <Address>{state.address}</Address>
+        </Text>
+        {state.comment && <Text>Comment: "{state.comment}"</Text>}
 
-      <H3>Network fee estimation</H3>
-      <Gap />
+        <H3>Network fee estimation</H3>
+        <Gap />
 
-      <ButtonBottomRow>
-        <CancelButton />
-        <ButtonPositive onClick={onConfirm}>Confirm</ButtonPositive>
-      </ButtonBottomRow>
-    </Body>
+        <ButtonBottomRow>
+          <CancelButton />
+          <ButtonPositive onClick={onConfirm}>Confirm</ButtonPositive>
+        </ButtonBottomRow>
+      </Body>
+    </>
   );
 };
 
@@ -153,17 +185,12 @@ const LoadingView = () => {
 };
 
 export const Send: FC<Props> = ({ price, balance }) => {
-  const [searchParams, setSearchParams] = useSearchParams({});
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const submit = searchParams.get("submit") === "1";
 
   const state = useMemo(() => {
-    return {
-      address: searchParams.get("address") ?? "",
-      amount: searchParams.get("amount") ?? "",
-      max: searchParams.get("max") ?? "",
-      comment: searchParams.get("comment") ?? "",
-    };
+    return toState(searchParams);
   }, [searchParams]);
 
   const onSend = useCallback(() => {
