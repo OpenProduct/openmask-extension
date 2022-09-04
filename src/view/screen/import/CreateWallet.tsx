47bded469a0c91c59428edd1918c6db664b549ee
Nikita Kuznetsov <nkuz915@gmail.com>
2022-09-04 21:59:15 +0200

    RememberMnemonic screen

--- src/view/screen/import/CreateWallet.tsx
+++ src/view/screen/import/CreateWallet.tsx
@@ -1,22 +1,28 @@
-import { useEffect, useState } from "react";
+import React, { FC, useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import * as tonMnemonic from "tonweb-mnemonic";
 import {
   Body,
+  ButtonColumn,
   ButtonNegative,
   ButtonPositive,
   ButtonRow,
+  Container,
   ErrorMessage,
   H1,
+  Input,
   Text,
   Textarea,
 } from "../../components/Components";
+import { BackIcon } from "../../components/Icons";
 import { AppRoute } from "../../routes";
 import { useCreateWalletMutation } from "./api";
 
 export const Create = () => {
   const navigate = useNavigate();
   const [mnemonic, setMnemonic] = useState("");
+  const [test, setTest] = useState(false);
   const [show, setShow] = useState(false);
 
   const { mutateAsync, reset, isLoading } = useCreateWalletMutation();
@@ -34,14 +40,25 @@ export const Create = () => {
       setShow(true);
       return;
     }
-    onCreate();
+    setTest(true);
   };
 
   const onCreate = async () => {
+    setTest(false);
     reset();
     await mutateAsync(mnemonic);
     navigate(AppRoute.home);
   };
+
+  if (test) {
+    return (
+      <RememberMnemonic
+        mnemonic={mnemonic}
+        onConfirm={onCreate}
+        onBack={() => setTest(false)}
+      />
+    );
+  }
 
   return (
     <Body>
@@ -57,8 +74,11 @@ export const Create = () => {
       <Textarea disabled rows={8} value={show ? mnemonic : ""} />
       <Text>OpenMask cannot recover your Secret Recovery Phrase.</Text>
       <ButtonRow>
-        <ButtonNegative disabled={disabled} onClick={onCreate}>
-          Skip
+        <ButtonNegative
+          disabled={isLoading}
+          onClick={() => navigate(AppRoute.home)}
+        >
+          Cancel
         </ButtonNegative>
         <ButtonPositive disabled={disabled} onClick={onShow}>
           {show ? "Create" : "Show"}
@@ -68,7 +88,76 @@ export const Create = () => {
   );
 };
 
-// https://metamask.zendesk.com/hc/en-us/articles/360015489591-Basic-Safety-Tips
-// Never share your Secret Recovery Phrase with anyone The MetaMask team will
-// never ask for your Secret Recovery Phrase Always keep your Secret Recovery
-// Phrase in a secure and secret place
+const Block = styled(Container)`
+  width: 100%;
+`;
+const Button = styled.div`
+  cursor: pointer;
+`;
+
+const Label = styled.div`
+  font-size: medium;
+`;
+
+type RememberProps = {
+  mnemonic: string;
+  onConfirm: () => void;
+  onBack: () => void;
+};
+
+function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+export const RememberMnemonic: FC<RememberProps> = React.memo(
+  ({ mnemonic, onConfirm, onBack }) => {
+    const items = mnemonic.split(" ");
+
+    const [one, setOne] = useState("");
+    const [two, setTwo] = useState("");
+    const [three, setThree] = useState("");
+
+    const [test1, test2, test3] = useMemo(() => {
+      return [getRandomInt(1, 8), getRandomInt(8, 16), getRandomInt(16, 24)];
+    }, []);
+
+    const isValid =
+      one.toLowerCase().trim() === items[test1 - 1] &&
+      two.toLowerCase().trim() === items[test2 - 1] &&
+      three.toLowerCase().trim() === items[test3 - 1];
+
+    return (
+      <>
+        <Block>
+          <Button onClick={onBack}>
+            <BackIcon /> Back
+          </Button>
+        </Block>
+        <Body>
+          <H1>Secret Recovery Phrase</H1>
+          <Text>
+            Now let's check that you wrote your secret words correctly
+          </Text>
+          <Text>Please enter the words:</Text>
+          <ButtonColumn>
+            <label>
+              <Label>Word {test1}</Label>
+              <Input value={one} onChange={(e) => setOne(e.target.value)} />
+            </label>
+            <label>
+              <Label>Word {test2}</Label>
+              <Input value={two} onChange={(e) => setTwo(e.target.value)} />
+            </label>
+            <label>
+              <Label>Word {test3}</Label>
+              <Input value={three} onChange={(e) => setThree(e.target.value)} />
+            </label>
+            <ButtonPositive disabled={!isValid} onClick={onConfirm}>
+              Confirm
+            </ButtonPositive>
+          </ButtonColumn>
+        </Body>
+      </>
+    );
+  }
+);
