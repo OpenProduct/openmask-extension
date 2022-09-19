9dba8bcdc686a219317993e7c7e2499de73e21c9
Nikita Kuznetsov <nkuz915@gmail.com>
2022-09-19 17:26:40 +0200

    Receive Jetton view

--- src/view/screen/home/wallet/assets/jetton/Jetton.tsx
+++ src/view/screen/home/wallet/assets/jetton/Jetton.tsx
@@ -1,11 +1,25 @@
-import { useContext, useMemo } from "react";
+import { FC, useContext, useMemo } from "react";
 import { Route, Routes, useParams } from "react-router-dom";
+import { Body } from "../../../../../components/Components";
+import { HomeButton } from "../../../../../components/HomeButton";
 import { WalletStateContext } from "../../../../../context";
+import { ReceiveCoin } from "../../receive/Receive";
 import { JettonMinterAddressContext, JettonStateContext } from "./context";
 import { JettonHide } from "./JettonHide";
 import { JettonHome } from "./JettonHome";
 import { JettonRoute } from "./route";
 import { JettonSend } from "./send/SendJetton";
+
+const ReceiveJetton: FC<{ symbol: string }> = ({ symbol }) => {
+  return (
+    <>
+      <HomeButton path="../" text="Back" />
+      <Body>
+        <ReceiveCoin symbol={symbol} />
+      </Body>
+    </>
+  );
+};
 
 export const JettonRouter = () => {
   const wallet = useContext(WalletStateContext);
@@ -26,7 +40,10 @@ export const JettonRouter = () => {
       <JettonMinterAddressContext.Provider value={minterAddress}>
         <Routes>
           <Route path={JettonRoute.send} element={<JettonSend />} />
-          <Route path={JettonRoute.receive} element={<div>Receive</div>} />
+          <Route
+            path={JettonRoute.receive}
+            element={<ReceiveJetton symbol={jetton.state.symbol} />}
+          />
           <Route path={JettonRoute.hide} element={<JettonHide />} />
           <Route path="*" element={<JettonHome />} />
         </Routes>
