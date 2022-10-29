c929004ad0508cb6accd95bb05cdc41b32c5b605
Nikita Kuznetsov <nkuz915@gmail.com>
2022-10-30 01:14:01 +0200

    Init parase transactions

--- src/view/screen/notifications/send/api.ts
+++ src/view/screen/notifications/send/api.ts
@@ -1,35 +1,124 @@
-import { base64ToBytes, Cell, fromNano } from "@openproduct/web-sdk";
+import {
+  Address,
+  base64ToBytes,
+  Cell,
+  fromNano,
+  Slice,
+} from "@openproduct/web-sdk";
 import { useQuery } from "@tanstack/react-query";
+import BN from "bn.js";
 import { TransactionParams } from "../../../../libs/entries/transaction";
 import { QueryType } from "../../../../libs/store/browserStore";
 import { TransactionState } from "../../home/wallet/send/api";
 
-const toData = (params: TransactionParams) => {
-  if (!params.data) return undefined;
+const toDataCell = (params: TransactionParams) => {
+  if (!params.data) return new Cell();
 
   switch (params.dataType) {
     case "hex":
       return Cell.oneFromBoc(params.data);
-    case "base64":
-      return base64ToBytes(params.data);
     case "boc":
       return Cell.oneFromBoc(base64ToBytes(params.data));
-    default:
-      return params.data;
+    case "base64": {
+      const bytes = base64ToBytes(params.data);
+      const payloadCell = new Cell();
+      payloadCell.bits.writeBytes(bytes);
+      return payloadCell;
+    }
+    default: {
+      const payloadCell = new Cell();
+      if (params.data.length > 0) {
+        payloadCell.bits.writeUint(0, 32);
+        payloadCell.bits.writeString(params.data);
+      }
+      return payloadCell;
+    }
   }
 };
 
+const seeIfSendNftTransaction = (payload: Cell): false | NftTransferState => {
+  try {
+    const slice: Slice = payload.beginParse();
+
+    const operator = slice.loadUint(32);
+    if (!operator.eq(new BN(0x5fcc3d14))) {
+      return false;
+    }
+
+    const queryId = slice.loadUint(64);
+
+    const newOwnerAddress = slice.loadAddress();
+    if (newOwnerAddress == null) {
+      return false;
+    }
+    const responseAddress = slice.loadAddress();
+    if (responseAddress === null) {
+      return false;
+    }
+
+    const customPayload = slice.loadBit() ? slice.loadRef().toCell() : null;
+
+    const forwardAmount = slice.loadCoins();
+
+    const forwardPayload = slice.loadBit()
+      ? slice.loadRef().toCell()
+      : slice.toCell();
+
+    return {
+      queryId,
+      newOwnerAddress,
+      responseAddress,
+      customPayload,
+      forwardAmount,
+      forwardPayload,
+    };
+  } catch (e) {
+    return false;
+  }
+};
+
+export interface NftTransferState {
+  queryId: BN;
+  newOwnerAddress: Address;
+  responseAddress: Address;
+  customPayload: Cell | null;
+  forwardAmount: BN;
+  forwardPayload: Cell;
+}
+
+export type ParsedTransactionState =
+  | { kind: "simple"; state: TransactionState }
+  | { kind: "nft"; state: TransactionState; nftTransfer: NftTransferState };
+
 export const useSendTransactionState = (params: TransactionParams) => {
-  return useQuery<TransactionState, Error>(
+  return useQuery<ParsedTransactionState, Error>(
     [QueryType.transactions, params],
     () => {
-      return {
+      const dataCell = toDataCell(params);
+
+      const state: TransactionState = {
         address: params.to,
         amount: fromNano(params.value),
         max: "0",
-        data: toData(params),
+        data: dataCell,
         hex: params.data,
       };
+
+      const nftTransfer = seeIfSendNftTransaction(dataCell);
+      if (nftTransfer) {
+        const result: ParsedTransactionState = {
+          kind: "nft",
+          nftTransfer,
+          state,
+        };
+        return result;
+      }
+
+      const result: ParsedTransactionState = {
+        kind: "simple",
+        state,
+      };
+      return result;
     }
   );
 };
