968fdee70e44fcb43271a864f33334017b144fa9
Witter <varik1770@gmail.com>
2024-06-23 18:58:24 +0800

    feat: support long message sign for PersonalSign

--- src/view/screen/notifications/sign/api.ts
+++ src/view/screen/notifications/sign/api.ts
@@ -26,6 +26,7 @@ export const usePersonalSignMutation = () => {
       throw new Error("Missing sign data");
     }
 
+    const valueHash = nacl.hash(Buffer.from(value, "utf8"));
     /**
      * According: https://github.com/ton-foundation/specs/blob/main/specs/wtf-0002.md
      */
@@ -37,7 +38,7 @@ export const usePersonalSignMutation = () => {
     const hex = Buffer.concat([
       Buffer.from([0xff, 0xff]),
       Buffer.from("ton-safe-sign-magic"),
-      Buffer.from(value, "utf8"),
+      valueHash,
     ]).toString("hex");
 
     const keyPair = await getWalletKeyPair(wallet);
